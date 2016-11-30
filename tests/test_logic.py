d0b9c1f833a73e4bcce2b6a129f40c27cec82b5f
Steve Kamau <stevekamau72@gmail.com>
2016-11-30 19:20:06 +0300

    Added a fucntion that divides

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -5,7 +5,10 @@ class TestCalculator(unittest.TestCase):
 	#Tests that our function adds correctly
 	def test_magic_sum(self):
 		self.assertEqual(magic_sum(5),15)
-       
+     
+          #tests that our function divides correctly
+	def test_magic_div(self):
+		self.assertEqual(magic_div(5),2)
 
 
 if __name__ == '__main__':
